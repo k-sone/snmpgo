355ded3d490b1a1f2ac6e8ebb949e23944491db1
Daniel Carbone <daniel.p.carbone@gmail.com>
2017-02-18 18:32:45 -0600

    Update log.go

--- log.go
+++ log.go
@@ -3,13 +3,4 @@ package snmpgo
 type StdLogger interface {
 	Print(v ...interface{})
 	Printf(format string, v ...interface{})
-	Println(v ...interface{})
-
-	Fatal(v ...interface{})
-	Fatalf(format string, v ...interface{})
-	Fatalln(v ...interface{})
-
-	Panic(v ...interface{})
-	Panicf(format string, v ...interface{})
-	Panicln(v ...interface{})
 }
